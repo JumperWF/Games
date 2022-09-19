0dce85aa480b472044d7527dbfe72b6f0944e5a1
Dmitrii <81853833+JumperWF@users.noreply.github.com>
2022-09-19 20:37:48 +0300

    Add files via upload
    helpful function to make a word to play Hangman

---
+++ hangman.js
@@ -0,0 +1,13 @@
+function prepareWordForHangman(word1){
+    if (typeof word1 === 'string' && word1!=""){
+        if(word1.length>1){
+            var str = ``;
+                for (var i = word1.length-3; i >= 0; i--){
+                str = str + `-`;
+            }
+            var hyphens = str;
+            console.log(word1[0].toUpperCase()+hyphens+word1[word1.length-1].toUpperCase())
+        } else console.log(`ERROR`)
+    } else console.log(`ERROR`)
+}
+prepareWordForHangman(`cat`)
