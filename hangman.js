c8d279d4d36c138e4a97f1b5fb5fff189634988c
Dmitrii <81853833+JumperWF@users.noreply.github.com>
2022-09-20 09:43:49 +0300

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,4 +1,4 @@
-function prepareWordForHangman(word1){
+function prepareWordForHangman1(word1){
     if (typeof word1 === 'string' && word1.length>2){
         var str = ``;
         for (var i = word1.length-3; i >= 0; i--){
@@ -8,4 +8,17 @@ function prepareWordForHangman(word1){
         console.log(word1[0].toUpperCase()+hyphens+word1[word1.length-1].toUpperCase())
     } else console.log(`ERROR`)
 }
-prepareWordForHangman("ololo")
+prepareWordForHangman1("ololo")
+
+
+
+//_________________________________________________
+
+function prepareWordForHangman2(word1){
+    if (typeof word1 === 'string' && word1.length>2){
+        let str = ""
+        str += ` - `.repeat(word1.length - 2)
+        console.log(word1[0].toUpperCase()+str+word1[word1.length-1].toUpperCase())
+    } else console.log(`ERROR`)
+}
+prepareWordForHangman2("cat")
